11de53067bece99d221531c2075c888d33b4257f
Yorí Galisteu <yorigcsdev@gmail.com>
2024-02-17 00:55:11 -0300

    feat: add sqlx:test

--- tests/subscriptions.rs
+++ tests/subscriptions.rs
@@ -1,14 +1,17 @@
 use axum::{extract::{Request}, http::{self, StatusCode}, body::Body};
 use tower::ServiceExt;
 use mime;
+use sqlx::{Pool, Postgres};
+
 mod helpers;
 use helpers::TestApp;
-#[tokio::test]
-async fn subscribe_returns_a_200_for_valid_form_data() {
-    let body = String::from("name=yori%20galisteu&email=yorigcs%40gmail.com");
-    let TestApp{ db_pool, app}  = TestApp::new().await;
 
-    let response = app.with_state(db_pool.clone())
+#[sqlx::test]
+async fn subscribe_returns_a_200_for_valid_form_data(pool: Pool<Postgres>) {
+    let body = String::from("name=yori%20galisteu&email=yorigcs%40gmail.com");
+    let TestApp{app, .. }  = TestApp::new().await;
+
+    let response = app.with_state(pool.clone())
         .oneshot(
             Request::builder()
                 .method(http::Method::POST)
@@ -20,7 +23,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     assert_eq!(response.status(), StatusCode::OK);
 
     let saved = sqlx::query!("SELECT email, name FROM subscriptions")
-        .fetch_one(&db_pool)
+        .fetch_one(&pool)
         .await
         .expect("Failed to fetch save subscription");
 
